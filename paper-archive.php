1e50442537ee5d3193a1b89d04ca37abada6e3f3
Ulf Wüstefeld <ulf@wuesti.eu>
2022-11-13 16:06:52 +0100

    Tried some update features, edited info json file

--- paper-archive.php
+++ paper-archive.php
@@ -3,41 +3,12 @@
 Plugin Name: Paper Archive
 Plugin URI: https://www.wuesti.eu/wp-paperarchive
 Description: Lists PDF files from media gallery based on a conected page.
-Version: 1.1
+Version: 1.2
 Author: Ulf Wüstefeld
 Author URI: https://www.wuesti.eu
 License: GPLv2
 Update URI: https://www.wuesti.eu/paper-archive/paper-archive.json
 */
-
-/* update */
-if( ! function_exists( 'my_plugin_check_for_updates' ) ){
-    
-    function my_plugin_check_for_updates( $update, $plugin_data, $plugin_file ){
-        
-        static $response = false;
-        
-        if( empty( $plugin_data['UpdateURI'] ) || ! empty( $update ) )
-            return $update;
-        
-        if( $response === false )
-            $response = wp_remote_get( $plugin_data['UpdateURI'] );
-        
-        if( empty( $response['body'] ) )
-            return $update;
-        
-        $custom_plugins_data = json_decode( $response['body'], true );
-        
-        if( ! empty( $custom_plugins_data[ $plugin_file ] ) )
-            return $custom_plugins_data[ $plugin_file ];
-        else
-            return $update;
-        
-    }
-    
-    add_filter('update_plugins_wuesti.eu', 'my_plugin_check_for_updates', 10, 3);
-    
-}
 
 /* shortcode */
 function fn_pdffiles($atts, $content = null)
